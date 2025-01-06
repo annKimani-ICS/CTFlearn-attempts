f1d9572dbf46548ca842983ff268186afd203476
annkimani <ann.wangari@strathmore.edu>
2025-01-06 11:39:58 +0300

    Initial commit

---
+++ challenges/simple_programming.py
@@ -0,0 +1,8 @@
+import math
+
+file_path = "C:\Users\Ann Wangari\Downloads\data.dat"
+
+#initialize counter for lines
+count = 0
+
+# Check if the count of 0's is a multiple of 3 or the count of 1's is a multiple of 2
