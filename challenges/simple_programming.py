c8ced14cd1356bedd53117da686fc1e0289507ee
annkimani <ann.wangari@strathmore.edu>
2025-01-06 12:42:40 +0300

    Final solved code using open. DO NOT use line package, full of errors

--- challenges/simple_programming.py
+++ challenges/simple_programming.py
@@ -1,18 +1,14 @@
-import line
+#open data file
+file_path = "../../../Downloads/data.txt"  #file path for the data we're working with
 
-file_path = "../../../Downloads/data.dat"  #file path for the data we're working with
+Lines = 0
+with open(file_path, 'r') as file:
+    #iterate through each line and count the number of zeros and ones
+    for line in file:
+        count_zeros = line.count("0")
+        count_ones = line.count("1")
 
-#initialize counter for lines
-count = 0
-lines = file_path.readlines()
-
-# Strip whitespace from the line and check if it's not empty
-if line.strip():
-    for line in lines:  #check for the number of zeros and ones
-        count_zeros = line.count('0')
-        count_ones = line.count('1')
-
-# Check if the count of 0's is a multiple of 3 or the count of 1's is a multiple of 2
-if count_zeros % 3 or count_ones % 2:
-    count += 1 #increment counter if condition is met
-    print(count)
+        #if condition is met increment the counter by one
+        if count_ones % 2 == 0 or count_zeros % 3 == 0:
+            Lines += 1
+            print(Lines)
