b5c31d4bc7d5971d3bb50522b41728e86fb42e85
annkimani <ann.wangari@strathmore.edu>
2025-01-06 11:56:30 +0300

    Line by line counter and multiples checker

--- challenges/simple_programming.py
+++ challenges/simple_programming.py
@@ -1,8 +1,18 @@
-import math
+import line
 
-file_path = "C:\Users\Ann Wangari\Downloads\data.dat"
+file_path = "../../../Downloads/data.dat"  #file path for the data we're working with
 
 #initialize counter for lines
 count = 0
+lines = file_path.readlines()
+
+# Strip whitespace from the line and check if it's not empty
+if line.strip():
+    for line in lines:  #check for the number of zeros and ones
+        count_zeros = line.count('0')
+        count_ones = line.count('1')
 
 # Check if the count of 0's is a multiple of 3 or the count of 1's is a multiple of 2
+if count_zeros % 3 or count_ones % 2:
+    count += 1 #increment counter if condition is met
+    print(count)
